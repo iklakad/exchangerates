9da01b64a78c5758b9660b68ace855a4dbf3502d
iklakad <rakeshsaii@gmail.com>
2024-05-31 16:22:08 +1000

    python changes
    comments added

--- Extract_Load_Json.py
+++ Extract_Load_Json.py
@@ -5,7 +5,7 @@ import json
 Exchangerate_RawData = requests.get('https://api.exchangeratesapi.io/v1/timeseries?access_key=e220123abca072517e7d5620224964e9&start_date=2024-04-29&end_date=2024-05-29&base=AUD&symbols=AUD,NZD,USD')
 Exchangerate_RawData = Exchangerate_RawData.json()
 #to view the data
-print(Exchangerate_RawData)
+print("The raw data looks as below:\n" ,Exchangerate_RawData)
 #save the json file
 with open('Exchangerate_RawData.json','w', encoding='utf-8') as file:
     json.dump(Exchangerate_RawData,file,indent=4)
